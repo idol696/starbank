2f1f26b26c0567aee8e8c0263630cbb6bd5f13d4
Roman Kuzmin <ghelatin@mail.ru>
2025-01-18 01:56:59 +0300

    commit init

---
+++ src/main/java/com/skypro/starbank/StarBankApplication.java
@@ -0,0 +1,14 @@
+package com.skypro.starbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarBankApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(StarBankApplication.class, args);
+	}
+
+}
