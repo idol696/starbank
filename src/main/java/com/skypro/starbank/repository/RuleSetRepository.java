e1003bb67c1db26b795bf29f547cf0faf3840a45
Roman Kuzmin <ghelatin@mail.ru>
2025-01-29 19:38:12 +0300

    feat: добавлен кэш Caffeine fix: добавлены зависимости под кэш в файл pom.xml fix: внесены правки по кэшу в RuleSetRepository feat: добавлен класс управления кэшем CacheConfig.java

--- src/main/java/com/skypro/starbank/repository/RuleSetRepository.java
+++ src/main/java/com/skypro/starbank/repository/RuleSetRepository.java
@@ -1,6 +1,7 @@
 package com.skypro.starbank.repository;
 
 import com.skypro.starbank.model.rules.RuleSet;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +9,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Repository
+@Cacheable(value = "rules", key = "#productId")
 public interface RuleSetRepository extends JpaRepository<RuleSet, Long> {
     Optional<RuleSet> findByProductId(UUID productId);
 }
