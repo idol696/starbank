709c9b7ba91eaba16b982ec0ed49d26d7a08b3f7
Roman Kuzmin <ghelatin@mail.ru>
2025-02-04 23:23:03 +0300

    fix: исправлен заголовок Swagger

--- src/main/java/com/skypro/starbank/configuration/SwaggerConfig.java
+++ src/main/java/com/skypro/starbank/configuration/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package com.skypro.starbank.configuration;
 
+import com.skypro.starbank.service.ManagementService;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springdoc.core.models.GroupedOpenApi;
@@ -9,12 +10,18 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SwaggerConfig {
 
+    public final  ManagementService managementService;
+
+    public SwaggerConfig(ManagementService managementService) {
+        this.managementService = managementService;
+    }
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("StarBank API")
-                        .version("1.0")
+                        .title(managementService.getServiceInfo().get("name"))
+                        .version(managementService.getServiceInfo().get("version"))
                         .description("API для управления StarBank Recommended Program for SkyPro School"));
     }
 
