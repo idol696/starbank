456ad1ec614b8f9bdb45fb134aae2c0dcd32ab26
Lins <alina.cheremiskina@gmail.com>
2025-02-11 19:18:31 +0300

    feat, refactor: улучшение обработки исключений и документации в сервисе Telegram-бота
    - Переработана логика обработки UserNotFoundException:
      * Добавлена возможность передачи кастомного сообщения в конструктор исключения.
      * Улучшена информативность выбрасываемых ошибок с указанием имени пользователя.
    
    - Улучшен сервис TelegramBotServiceImpl:
      * Добавлено подробное логирование при возникновении исключения UserNotFoundException.
      * Реализована более четкая обработка случая, когда пользователь не найден.
    
    - Добавлена документация (Javadoc) для класса и всех его методов:
      * Описаны назначение класса, параметры методов, возможные исключения и возвращаемые значения.
      * Улучшена читаемость кода благодаря детальной документации.
    
    - Внедрено логирование через SLF4J:
      * Добавлены записи об ошибках для упрощения диагностики проблем.
      * Логируется имя пользователя при возникновении исключения.

--- src/main/java/com/skypro/starbank/exception/UserNotFoundException.java
+++ src/main/java/com/skypro/starbank/exception/UserNotFoundException.java
@@ -5,7 +5,21 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends RuntimeException {
+
+    /**
+     * Конструктор без параметров.
+     */
     public UserNotFoundException() {
-        super("UserNotFound");
+        super("User not found");
+    }
+
+    /**
+     * Конструктор с кастомным сообщением.
+     *
+     * @param message Сообщение об ошибке.
+     */
+    public UserNotFoundException(String message) {
+        super(message);
     }
 }
+
--- src/main/java/com/skypro/starbank/service/TelegramBotServiceImpl.java
+++ src/main/java/com/skypro/starbank/service/TelegramBotServiceImpl.java
@@ -1,6 +1,5 @@
 package com.skypro.starbank.service;
 
-import com.skypro.starbank.events.MessageEvent;
 import com.skypro.starbank.events.TelegramMessageEvent;
 import com.skypro.starbank.exception.UserNotFoundException;
 import com.skypro.starbank.model.RecommendationResponse;
@@ -14,29 +13,59 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Реализация сервиса для работы с Telegram-ботом.
+ * Этот класс отвечает за обработку запросов пользователя,
+ * получение рекомендаций из сервиса рекомендаций и отправку сообщений через событие.
+ */
 @Service
 public class TelegramBotServiceImpl implements BotService {
 
+    /**
+     * Логгер для записи информации о работе бота.
+     */
     private static final Logger logger = LoggerFactory.getLogger(TelegramBotServiceImpl.class);
+
+    /**
+     * Сервис для получения рекомендаций пользователю.
+     */
     private final RecommendationService recommendationService;
+
+    /**
+     * Публикация событий для отправки сообщений через Telegram.
+     */
     private final ApplicationEventPublisher eventPublisher;
 
-
+    /**
+     * Конструктор для внедрения зависимостей.
+     *
+     * @param recommendationService Сервис для получения рекомендаций.
+     * @param eventPublisher        Публикатор событий для отправки сообщений.
+     */
     public TelegramBotServiceImpl(RecommendationService recommendationService,
                                   ApplicationEventPublisher eventPublisher) {
         this.recommendationService = recommendationService;
         this.eventPublisher = eventPublisher;
     }
 
+    /**
+     * Получает рекомендации для пользователя по его имени.
+     * Если пользователь не найден, выбрасывает UserNotFoundException.
+     *
+     * @param username Имя пользователя.
+     * @return Строковое представление рекомендаций.
+     * @throws UserNotFoundException Если пользователь не найден.
+     */
     @Transactional
-    public String getBotRecommendationByUsername(String username) {
+    public String getBotRecommendationByUsername(String username) throws UserNotFoundException {
         RecommendationResponse recommendations;
         try {
             recommendations = recommendationService.getRecommendationsByUserName(username);
         } catch (UserNotFoundException e) {
-            return "Пользователь не найден, проверьте правильность написания имени и фамилии";
+            // Логируем ошибку и перебрасываем её дальше
+            logger.error("Пользователь {} не найден", username, e);
+            throw new UserNotFoundException(String.format("Пользователь %s не найден", username));
         }
-
         StringBuilder response = new StringBuilder();
         response.append("Здравствуйте ").append(username).append("\n");
         response.append("Новые продукты для вас:\n");
@@ -52,20 +81,32 @@ public class TelegramBotServiceImpl implements BotService {
         return response.toString();
     }
 
-
-        public List<String> giveMessageArguments (String messageText){
-            return Arrays.stream(messageText.split("[.\s|]+"))
-                    .skip(1)
-                    .filter(arg -> arg.matches("[a-zA-Zа-яА-Я0-9]+"))
-                    .toList();
-        }
-
-        @Override
-        public void sendMessage ( long chatId, String text){
-            SendMessage message = new SendMessage();
-            message.setChatId(String.valueOf(chatId));
-            message.setText(text);
-
-            eventPublisher.publishEvent(new TelegramMessageEvent(this, message));
-        }
+    /**
+     * Разбивает текст сообщения на список аргументов.
+     * Удаляет лишние пробелы и символы пунктуации, оставляя только буквы и цифры.
+     *
+     * @param messageText Исходный текст сообщения.
+     * @return Список аргументов из текста сообщения.
+     */
+    public List<String> giveMessageArguments(String messageText) {
+        return Arrays.stream(messageText.split("[.\s|]+"))
+                .skip(1)
+                .filter(arg -> arg.matches("[a-zA-Zа-яА-Я0-9]+"))
+                .toList();
     }
+
+    /**
+     * Отправляет сообщение через Telegram.
+     * Создает объект {@link SendMessage} и публикует событие для отправки сообщения.
+     *
+     * @param chatId ID чата, куда нужно отправить сообщение.
+     * @param text   Текст сообщения.
+     */
+    @Override
+    public void sendMessage(long chatId, String text) {
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(text);
+        eventPublisher.publishEvent(new TelegramMessageEvent(this, message));
+    }
+}
